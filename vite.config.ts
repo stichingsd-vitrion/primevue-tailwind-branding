fe43f95e7fb902ae553714a8e2d135088291e6e4
Dion <dion@vitrion.nl>
2024-07-30 19:28:04 +0200

    ✨ Adds homepage, predeploy, and deploy scripts in package.json, and updates dependencies for gh-pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/primevue-tailwind-branding/",
     plugins: [
         vue(),
         Components({
